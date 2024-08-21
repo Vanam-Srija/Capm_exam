3ceea2523f8d11f9d3eac20e1dea1c10f2a94570
Srija Vanam <srija.vanam@ust.com>
2024-08-21 09:18:39 +0000

    changes in capm exam

--- srv/EmployeeService.js
+++ srv/EmployeeService.js
@@ -16,19 +16,33 @@ module.exports = cds.service.impl( async function(){
     this.before('UPDATE', EmployeeSet, (req) => {
         var salary = parseInt(req.data.salaryAmount);
         if(salary < 50000 && Currency_code==USD){
-            console.log("Create operation successful");
+            console.log("Update operation successful");
         }
         else{
-            req.error(500,"Employee’s salary must be less than 50000 USD");
+            req.error(500,"Update operation is not allowed for nameFirst and loginName");
         }
     });
 
-    this.on('DELETE', EmployeeSet,(req)=>{
-    })
+
+    this.on('DELETE', EmployeeSet, async (req) => {
+        const employeeId = req.params[0];
+        
+        const employee = await SELECT.one.from(EmployeeSet).where({ ID: employeeId });
+        
+        
+        if (!employee.nameFirst.startsWith('S')) {
+            return req.error(500, 'Deletion is not allowed first name starts with "S"');
+        }
+        
+        await DELETE.from(EmployeeSet).where({ ID: employeeId });
+        
+        console.log("Deletion operation is sucessfull");
+        
+        return;
+    });
 
 
- 
-}
-);
+});
+
 
  
