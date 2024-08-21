d406246daae14c1db92f0429c37b68f5c8b9993f
Srija Vanam <srija.vanam@ust.com>
2024-08-21 10:48:24 +0000

    capm exam code

--- srv/EmployeeService.js
+++ srv/EmployeeService.js
@@ -6,43 +6,31 @@ module.exports = cds.service.impl( async function(){
 //During creation validating if the employee salary is less than 50000 or not
     this.on('CREATE', EmployeeSet, (req) => {
         var salary = parseInt(req.data.salaryAmount);
-        if(salary < 50000 && Currency_code==USD){
+        var currency_code = req.data.Currency;
+        if(salary < 50000 && currency_code == USD){
             console.log("Create operation successful");//creation sucess meassage
         }
         else{
             req.error(500,"Employee’s salary must be less than 50000 USD"); //error message thrown
         }
     });
-    this.before('UPDATE', EmployeeSet, (req) => {
-        var salary = parseInt(req.data.salaryAmount);
-        if(salary < 50000 && Currency_code==USD){
-            console.log("Update operation successful");
+    
+    this.before('UPDATE',EmployeeSet,(req)=>{
+        if(('nameFirst' in req.data) && ('loginName' in req.data) ){
+                req.error("Operation not allowed");
         }
-        else{
-            req.error(500,"Update operation is not allowed for nameFirst and loginName");
-        }
+    });
+ 
+    this.after('UPDATE', EmployeeSet, (req) => {
+        console.log("Update operation successful");
     });
 
 
-    this.on('DELETE', EmployeeSet, async (req) => {
-        const employeeId = req.params[0];
-        
-        const employee = await SELECT.one.from(EmployeeSet).where({ ID: employeeId });
-        
-        
-        if (!employee.nameFirst.startsWith('S')) {
-            return req.error(500, 'Deletion is not allowed first name starts with "S"');
-        }
-        
-        await DELETE.from(EmployeeSet).where({ ID: employeeId });
-        
-        console.log("Deletion operation is sucessfull");
-        
-        return;
-    });
-
-
+    this.on('DELETE',EmployeeSet,(req)=>{
+        console.log(" Delete operation is successful");
+    })
 });
+
 
 
  
